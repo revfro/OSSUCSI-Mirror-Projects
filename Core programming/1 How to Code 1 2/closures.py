b80e754c7b32f40e8f9f2e9675d48425ed1b821f
Gabriel S. Lindor <gslindor@icloud.com>
2022-06-07 13:30:17 -0400

    June 7th update

---
+++ Core programming/1 How to Code 1 2/closures.py
@@ -0,0 +1,35 @@
+import unittest
+
+class Image:
+	def __init__(self,a,b,c,d):
+		self.width = a
+		self.height = b
+		self.fill = c
+		self.colour = d
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Testing
+
+class FnTest(unittest.TestCase):
+    #
+    def testBaseCase(self):
+        self.assertEqual(fn(base), expected)
+    def testAValue(self):
+        self.assertEqual(fn(val1), expected)
+    def testA2ndValue(self):
+        self.assertEqual(fn(val2), expected)
+    #
+
+if __name__ == '__main__':
+	unittest.main()
